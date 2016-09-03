ecfd4640dadee7fdb7737aa792d6fecb686bfc22
pookzzz <w.kuttiya@gmail.com>
2016-09-03 17:01:11 +0000

    todos get post

--- todo.rb
+++ todo.rb
@@ -1,7 +1,31 @@
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" if development?
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
-  erb "You have no lists.", layout: :layout
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists
+end
+
+get "/lists/new" do
+  erb :new_list
+end
+
+post "/lists" do
+  session[:lists] << {name: params[:list_name], todos: []}
+  session[:success] = "The list has been created."
+  redirect "/lists"
 end
