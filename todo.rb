1b444838f63a12c66d63f2a78c1ead9010437578
pookzzz <w.kuttiya@gmail.com>
2016-09-04 18:37:33 +0000

    add edit

--- todo.rb
+++ todo.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader" if development?
+require "sinatra/content_for"
 require "tilt/erubis"
 
 configure do
@@ -24,8 +25,54 @@ get "/lists/new" do
   erb :new_list
 end
 
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "The list name must be between 1 and 100 characters."
+  elsif session[:lists].any? {|list| list[:name] == name}
+    "List name must be unique."
+  end
+end
+
 post "/lists" do
-  session[:lists] << {name: params[:list_name], todos: []}
-  session[:success] = "The list has been created."
-  redirect "/lists"
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list
+  else
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+end
+
+get "/lists/:id" do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  erb :list
+end
+
+get "/lists/:id/edit" do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  erb :edit_list
+end
+
+post "/lists/:id" do
+  list_name = params[:list_name].strip
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list
+  else
+    @list[:name] = list_name
+    session[:success] = "The list has been updated."
+    redirect "/lists/#{id}"
+  end
 end
